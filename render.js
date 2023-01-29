0134057c63b76ebc002fce8b403cba5f2805753d
islinxuan <islinxuan@outlook.com>
2023-01-29 22:15:09 +0800

    feat: Fibers

--- render.js
+++ render.js
@@ -1,15 +1,88 @@
-function render(container, element) {
-  const dom = element.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type);
+function createDom(fiber) {
+  const dom = fiber.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(fiber.type);
 
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter((key) => key !== "children")
-    .forEach((key) => (dom[key] = element.props[key]));
+    .forEach((key) => (dom[key] = fiber.props[key]));
 
-  element.props.children.forEach((child) => {
-    render(dom, child);
-  });
+  return dom;
+}
 
-  container.appendChild(dom);
+// Start working on the root.
+function render(container, element) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+// React doesn’t use requestIdleCallback anymore.
+// Now it uses the scheduler package.
+
+// Concurrent Mode
+// ===============
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  // 1. add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.append(fiber.dom);
+  }
+
+  // 2. create new fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibing = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    // whether it's the 1st child or not
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibing.sibling = newFiber;
+    }
+
+    prevSibing = newFiber;
+    index++;
+  }
+
+  // 3. return next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 export { render };
