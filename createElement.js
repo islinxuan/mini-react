19b194d69efd407adfef5d6499db93ee303e2274
islinxuan <islinxuan@outlook.com>
2023-01-29 18:25:23 +0800

    feat: the createElement function

---
+++ createElement.js
@@ -0,0 +1,24 @@
+// React doesn’t wrap primitive values
+// or create empty arrays when there aren’t children
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => (typeof child === "object" ? child : createTextElement(child))),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+export { createElement };
