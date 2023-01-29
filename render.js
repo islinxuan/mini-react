e781fad31ccd25fb2c15d42d8f1ca1402d8ac742
islinxuan <islinxuan@outlook.com>
2023-01-30 01:48:58 +0800

    feat: reconciliation

--- render.js
+++ render.js
@@ -1,5 +1,65 @@
-import { commitRoot } from "./commit.js";
+// Commit Phase
+// ============
+function commitRoot() {
+  deletions.forEach(commitWork);
+  commitWork(wipRoot.child);
+  currentRoot = wipRoot;
+  wipRoot = null;
+}
 
+function commitWork(fiber) {
+  if (!fiber) return;
+
+  const domParent = fiber.parent.dom;
+  if (fiber.effectTag === "PLACEMENT") {
+    domParent.append(fiber.dom);
+  } else if (fiber.effectTag === "DELETION") {
+    fiber.dom.remove();
+  } else if (fiber.effectTag === "UPDATE") {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+  }
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
+const isEvent = (key) => key.startsWith("on");
+const isProperty = (key) => key !== "children" && !isEvent(key);
+const isNew = (prev, next) => (key) => prev[key] !== next[key];
+const isGone = (prev, next) => (key) => !next.hasOwnProperty(key);
+function updateDom(dom, prevProps, nextProps) {
+  // remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !nextProps.hasOwnProperty(key) || isNew(prevProps, nextProps)(key))
+    .forEach((key) => {
+      const eventType = key.toLowerCase().slice(2);
+      dom.removeEventListener(eventType, prevProps[key]);
+    });
+
+  // remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((key) => (dom[key] = ""));
+
+  // set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((key) => (dom[key] = nextProps[key]));
+
+  // add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((key) => {
+      const eventType = key.toLowerCase().slice(2);
+      dom.addEventListener(eventType, nextProps[key]);
+    });
+}
+
+// Render Phase
+// ============
 function createDom(fiber) {
   const dom = fiber.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(fiber.type);
 
@@ -10,14 +70,16 @@ function createDom(fiber) {
   return dom;
 }
 
-// Start working on the root.
+// start working on the root
 function render(container, element) {
   wipRoot = {
     dom: container,
     props: {
       children: [element],
     },
+    alternate: currentRoot,
   };
+  deletions = [];
   nextUnitOfWork = wipRoot;
 }
 
@@ -27,7 +89,9 @@ function render(container, element) {
 // Concurrent Mode
 // ===============
 let nextUnitOfWork = null;
+let currentRoot = null;
 let wipRoot = null;
+let deletions = null;
 
 function workLoop(deadline) {
   let shouldYield = false;
@@ -37,7 +101,7 @@ function workLoop(deadline) {
   }
 
   if (!nextUnitOfWork && wipRoot) {
-    commitRoot(wipRoot);
+    commitRoot();
   }
 
   requestIdleCallback(workLoop);
@@ -53,29 +117,7 @@ function performUnitOfWork(fiber) {
 
   // 2. create new fibers
   const elements = fiber.props.children;
-  let index = 0;
-  let prevSibing = null;
-
-  while (index < elements.length) {
-    const element = elements[index];
-
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    };
-
-    // whether it's the 1st child or not
-    if (index === 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibing.sibling = newFiber;
-    }
-
-    prevSibing = newFiber;
-    index++;
-  }
+  reconcileChildren(fiber, elements);
 
   // 3. return next unit of work
   if (fiber.child) {
@@ -87,6 +129,55 @@ function performUnitOfWork(fiber) {
       return nextFiber.sibling;
     }
     nextFiber = nextFiber.parent;
+  }
+}
+
+function reconcileChildren(wipFiber, elements) {
+  let index = 0;
+  let oldFiber = wipFiber.alternate?.child;
+  let prevSibling = null;
+
+  while (index < elements.length || oldFiber) {
+    const element = elements[index];
+    let newFiber = null;
+
+    // Here React also uses keys, that makes a better reconciliation.
+    // if there's any change
+    if (element?.type === oldFiber?.type) {
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE",
+      };
+    } else if (element) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT",
+      };
+    } else {
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    // whether it's the 1st child or not
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
   }
 }
 
